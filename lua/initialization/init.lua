c9b1c15366f3d428445f06ed25c3d4debebfef55
Joseph <QueszelEdu@gmail.com>
2026-01-16 10:16:13 -0400

    Initial Commit
    -Added defaults, keybinds, and lazy_init files to config directory for
    the initial loading of neovim

---
+++ lua/initialization/init.lua
@@ -0,0 +1,8 @@
+-- Default configurations
+require("initialization.configs.defaults")
+-- Package Manager
+require("initialization.configs.lazy_init")
+-- Load Keybinds
+require("initialization.configs.keybinds")
+
+
